04dd01459176081632ec1ce598aaf6e93b026a59
Lásaro Dumer <ladumer@hotmail.com>
2015-09-27 15:52:35 -0300

    New MEQsort version (work_index)

--- T_1/MEQsort.c
+++ T_1/MEQsort.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/time.h>
 #include "mpi.h"
 #define GET_WORK 0
 #define WORK_DONE 1
 #define WORK 2
 #define SUICIDE 3
-#define NUM_ARRAYS 1000
-#define ARRAYS_SIZE 10000
+#define WORK_INDEX 4
+#define NUM_ARRAYS 15
+#define ARRAYS_SIZE 8
 
 int compare (const void * a, const void * b){
     return ( *(int*)a - *(int*)b );
@@ -23,6 +25,8 @@ const char * printTag(int tag){
         return "WORK";
     }else if(tag==SUICIDE){
         return "SUICIDE";
+    }else if(tag==WORK_INDEX){
+        return "WORK_INDEX";
     }else{
         return "Invalid Tag";
     }
@@ -40,6 +44,7 @@ main(int argc, char** argv){
     int proc_n;
     int **saco;
     int toOrder[ARRAYS_SIZE];
+    int ordered[ARRAYS_SIZE];
     int i,j,s;
     int * val=(int*)0;
     double t1, t2;
@@ -67,6 +72,7 @@ main(int argc, char** argv){
             printf("out of memory\n");
             return -1;
         }else{
+            int valor = ARRAYS_SIZE * NUM_ARRAYS;
             for(i = 0; i < NUM_ARRAYS; i++)
             {
                 saco[i] =(int *) malloc(ARRAYS_SIZE * sizeof(int));
@@ -75,10 +81,14 @@ main(int argc, char** argv){
                     printf("out of memory row %d\n",i);
                     return -1;
                 }else{
+                    printf("array[%d]={",i);
                     for(j = 0; j< ARRAYS_SIZE; j++)
                     {
-                        saco[i][j] = ARRAYS_SIZE - j;
+                        saco[i][j] = valor;
+                        printf("%d,",saco[i][j]);
+                        valor--;
                     }
+                    printf("}\n");
                 }
             }
         }
@@ -88,10 +98,11 @@ main(int argc, char** argv){
         //printf("[%f]@sending works\n",curMilis());
         for(s=1;s<=slavesAlive;s++){
             if(next>=NUM_ARRAYS){
-                MPI_Send(val, 8, MPI_INT,s, SUICIDE, MPI_COMM_WORLD);
+                MPI_Send(&next, 1, MPI_INT,s, SUICIDE, MPI_COMM_WORLD);
                 slavesAlive--;
             }else {
                 //printf("[%f]@sending work to slave %d\n",curMilis(),s);
+                MPI_Send(&next,1,MPI_INT,s,WORK_INDEX,MPI_COMM_WORLD);
                 MPI_Send(saco[next], ARRAYS_SIZE, MPI_INT,s, WORK, MPI_COMM_WORLD);
                 next++;
             }
@@ -108,27 +119,48 @@ main(int argc, char** argv){
             printf("[%f]@master done ordering. total=%d/%d...\n",curMilis(),next,NUM_ARRAYS);
         }
         //*/
+        int orderedI = 0;
         while(slavesAlive > 0){
-        		MPI_Recv(toOrder, ARRAYS_SIZE, MPI_INT,MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);  // recebo por ordem de chegada com any_source
-        		if(status.MPI_TAG == WORK_DONE){
-        			saco[dones]=toOrder;
-        			dones++;
-        		}else if(status.MPI_TAG == GET_WORK){
-    	        	if(next>=NUM_ARRAYS){
-            				MPI_Send(toOrder, ARRAYS_SIZE, MPI_INT,status.MPI_SOURCE, SUICIDE, MPI_COMM_WORLD);
-            				slavesAlive--;
-                    }else {
-                        MPI_Send(saco[next], ARRAYS_SIZE, MPI_INT,status.MPI_SOURCE, WORK, MPI_COMM_WORLD);
-                        next++;
+    		MPI_Recv(&orderedI,1, MPI_INT,MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);  // recebo por ordem de chegada com any_source
+    		if(status.MPI_TAG == WORK_INDEX){
+                MPI_Recv(ordered, ARRAYS_SIZE, MPI_INT,status.MPI_SOURCE, WORK_DONE, MPI_COMM_WORLD, &status);  // recebo por ordem de chegada com any_source
+        		//saco[orderedI]=ordered;
+                memcpy(saco[orderedI],ordered,ARRAYS_SIZE*sizeof(int));
+                /*
+                printf("ordered[%d]={",dones);
+                for(j = 0; j< ARRAYS_SIZE; j++)
+                {
+                    printf("%d,",saco[dones][j]);
+                }
+                printf("}\n");//*/
+                /*
+                printf("ordenados\n");
+                for(i = 0; i < NUM_ARRAYS; i++)
+                {
+                    printf("array[%d]={",i);
+                    for(j = 0; j< ARRAYS_SIZE; j++)
+                    {
+                        printf("%d,",saco[i][j]);
                     }
-        		}
+                    printf("}\n");
+                }//*/
+    			dones++;
+	        	if(next>=NUM_ARRAYS){
+    				MPI_Send(&next, 1, MPI_INT,status.MPI_SOURCE, SUICIDE, MPI_COMM_WORLD);
+    				slavesAlive--;
+                }else {
+                    MPI_Send(&next,1,MPI_INT,status.MPI_SOURCE,WORK_INDEX,MPI_COMM_WORLD);
+                    MPI_Send(saco[next], ARRAYS_SIZE, MPI_INT,status.MPI_SOURCE, WORK, MPI_COMM_WORLD);
+                    next++;
+                }
+    		}
         }
         printf("[%f]@master leaving...\n",curMilis());
 
         t2 = MPI_Wtime();        // contagem de tempo termina neste ponto
 
         printf("[%f]@[%d]MPI_Wtime measured work time to be: %1.2f\n",curMilis(),my_rank, t2-t1);
-        /*
+        //*
         printf("ordenados\n");
         for(i = 0; i < NUM_ARRAYS; i++)
         {
@@ -143,20 +175,24 @@ main(int argc, char** argv){
     else
     {
         int tag = WORK;
+        int toOrderI=0;
         do{
             //printf("[%f]@slave[%d] waiting...\n",curMilis(),my_rank);
-            MPI_Recv(toOrder, ARRAYS_SIZE, MPI_INT,0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            //printf("[%f]@receiving work from master with tag %s\n",curMilis(),printTag(status.MPI_TAG));
-    		tag = status.MPI_TAG;
+            MPI_Recv(&toOrderI, 1, MPI_INT, 0, MPI_ANY_TAG,MPI_COMM_WORLD,&status);
+            tag = status.MPI_TAG;
+            if(tag == WORK_INDEX){
+                //printf("indice %d\n", toOrderI);
+                MPI_Recv(toOrder, ARRAYS_SIZE, MPI_INT,0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+                //printf("[%f]@receiving work from master with tag %s\n",curMilis(),printTag(status.MPI_TAG));
+        		tag = status.MPI_TAG;
+            }
     		if(tag == WORK){
-            qsort (toOrder, ARRAYS_SIZE, sizeof(int), compare);
-            MPI_Send(toOrder,  ARRAYS_SIZE, MPI_INT,0, WORK_DONE, MPI_COMM_WORLD);
-    		}
-            if(tag != SUICIDE){
-                MPI_Send(toOrder,ARRAYS_SIZE, MPI_INT,0, GET_WORK, MPI_COMM_WORLD);
+                qsort (toOrder, ARRAYS_SIZE, sizeof(int), compare);
+                MPI_Send(&toOrderI,1, MPI_INT,0, WORK_INDEX, MPI_COMM_WORLD);
+                MPI_Send(toOrder,ARRAYS_SIZE, MPI_INT,0, WORK_DONE, MPI_COMM_WORLD);
             }
         }while(tag != SUICIDE);
-        printf("[%f]@slave[%d] leaving...\n",curMilis(),my_rank);
+        //printf("[%f]@slave[%d] leaving...\n",curMilis(),my_rank);
     }
 
     MPI_Finalize();
